6ac40a2ca8b5cb8e5ad73c9658958779b6814d13
shah <shahjehan737@gmail.com>
2023-08-01 10:01:39 -0700

    ready

--- server.js
+++ server.js
@@ -1,55 +1,58 @@
-const express = require('express')
+const express = require('express');
+const { request } = require('http');
 const Datastore = require('nedb');
 const app = express()
-
 const PORT = 9000
 const HOST = '0.0.0.0';
-
 const database = new Datastore('drawings.db');
+
 database.loadDatabase();
 
 app.listen(PORT, HOST, () => {
     console.log(`Server has started on http://${HOST}:${PORT}`)
 })
-
 app.get('/chat', (req, res) => {
     const apiKey = process.env.api_key_chat;
     res.status(200).json({ info: apiKey })
 })
-
-app.get('/api', (request, response) => {
+app.get('/api', (req, res) => {
     database.find({}, (err, data) => {
         if (err) {
-            response.end();
+            res.end();
             return;
         }
-        response.json(data);
+        res.json(data);
     });
 });
-
-app.post('/api', (request, response) => {
-    const data = request.body;
+app.post('/api', (req, res) => {
+    const data = req.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     database.insert(data);
-    response.json(data);
+    res.json(data);
 });
-
-app.delete('/api/:id', (request, response) => {
+app.delete('/api/:id', (req, res) => {
     const postId = request.params.id;
     console.log(postId);
     database.remove({ _id: postId }, {}, (err, numRemoved) => {
         if (err) {
             console.error(`Error deleting post with ID ${postId}.`, err);
-            response.status(500).send(`Error deleting post with ID ${postId}.`);
+            res.status(500).send(`Error deleting post with ID ${postId}.`);
         } else {
             console.log(`Post with ID ${postId} deleted.`);
-            response.sendStatus(200);
+            res.sendStatus(200);
         }
     });
 });
-
 app.get('/health', (req, res) => {
-    const message = "";
-    res.status(200).json({ info: apiKey })
+    const message = "Healthy!";
+    res.status(200).json({ info: message })
+})
+app.get('/ready', (req, res) => {
+    const message = "Ready!";
+    res.status(200).json({ info: message })
+})
+app.post('db-test', (request, response) => {
+    database.insert({ name: 'Sheefamn', status: 'train' });
+    database.insert({ name: 'cat', status: 'alive' });
 })
