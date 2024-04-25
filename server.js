90759aeeeae31b84771715c274fbe7e81da96443
shah <shahjehan737@gmail.com>
2024-04-24 22:13:33 -0700

    changed cors

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const PORT = 9000
 const HOST = '0.0.0.0';
 
 // app.use(cors());
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://kubernetes.shahsportfolio.online'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Allowed HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allowed headers
+    res.setHeader('Access-Control-Allow-Credentials', true); // Allow credentials (e.g., cookies)
+    next();
+  });
 
 // Set the MIME type for JavaScript files
 app.set('view engine', 'js');
