7f79dbc2d17755b3b170b8cf3c8162a5c2e55b2d
shah <shahjehan737@gmail.com>
2024-05-17 20:54:35 -0700

    test

--- server.js
+++ server.js
@@ -11,17 +11,7 @@ const pgPass = process.env.POSTGRES_PASSWORD;
 const pgHost = process.env.DB_HOST;
 
 // Initialize Sequelize with your database connection
-const sequelize = new Sequelize(`${pgDb}`, `${pgUser}`, `${pgPass}`, {
-  host: `${pgHost}`,
-  port: 5432,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-});
+const sequelize = new Sequelize(`${pgDb}`, `${pgUser}`, `${pgPass}`, {host: `${pgHost}`, dialect: 'postgres', dialectOptions: {ssl: {require: true, rejectUnauthorized: false}}});
 
 const MyTable = sequelize.define('my_table', {
   _id: {
