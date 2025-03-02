5f6d75bf82792e9e505d145adb7d038c4f66166e
Shah <78382826+shahdevelopment@users.noreply.github.com>
2025-03-02 07:41:40 -0800

    Update server.js
    increased request limiit

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cookieParser());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Set the MIME type for JavaScript files
 app.set('view engine', 'js');
